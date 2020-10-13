88322eedbecb76410993684fbde85f10205e04bc
Lucille Yung Zhang <lucille.yungzhang@student.ie.edu>
2020-10-13 14:35:05 +0200

    add exercice.py file to git

---
+++ exercice.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 13 14:28:35 2020
+
+@author: LucilleYZ
+"""
+
+#%%
+
+def is_even(number):
+    if (number % 2) == 0:
+        return True
+    
+    
